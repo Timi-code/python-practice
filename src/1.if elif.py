c4ebca35f9c631bb6409f954c5e98999da7f59de
tangchengli <xiaoxiezhimu@qq.com>
2017-12-29 18:05:39 +0800

    -

--- src/1.if elif.py
+++ src/1.if elif.py
@@ -15,3 +15,20 @@ if "":
 else:
     print("'':false")
 
+
+height = input("身高：")
+weight = input("体重：")
+weight = int(weight)
+height = float(height)
+
+bmi = weight/(height*height)
+if bmi > 32:
+    print("严重肥胖")
+elif bmi > 28:
+    print("肥胖")
+elif bmi > 25:
+    print("过重")
+elif bmi > 18.5:
+    print("正常")
+else:
+    print("过轻")
