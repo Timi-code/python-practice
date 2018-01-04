8d2b0947387f43d30beb21408390e7ac41e01816
tangchengli <xiaoxiezhimu@qq.com>
2018-01-04 14:23:43 +0800

    过滤

---
+++ src/filter.py
@@ -0,0 +1,15 @@
+def is_odd(n):
+    return n % 2 == 1
+
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+c = list(filter(is_odd, l))
+print(c)
+print(l)
+
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+
+print(list(filter(is_palindrome, range(1, 100))))
