80ad8d7736851abce36a762dea8b0c52bf45f865
tangchengli <xiaoxiezhimu@qq.com>
2018-01-04 14:23:34 +0800

    排序

---
+++ src/sorted.py
@@ -0,0 +1,20 @@
+list = [36, 5, -12, 9, -21]
+
+print(sorted(list))
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+def by_score(t):
+    return -t[1]
+
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+L3 = sorted(L, key=by_score)
+print(L3)
